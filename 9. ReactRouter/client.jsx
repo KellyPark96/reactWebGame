7dc548b10745dd5813fed49905886b1db34382fd
KellyPark96 <kellypark2032@gmail.com>
2022-10-06 23:25:45 +0900

    webgame - 반응속도체크

---
+++ 9. ReactRouter/client.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import MineSearch from './MineSearch';
+// import ResponseCheck from './ResponseCheckClass';
+
+ReactDOM.createRoot(document.querySelector("#root")).render(<MineSearch />);
